dfa5b054ab8a1d32a6e5999043d3d32b82eb3496
Ahmed Mohamed Abozeid <157320734+Bozeid55@users.noreply.github.com>
2026-02-23 07:43:37 +0200

    Create P - Continents.cpp

---
+++ Juniors Phase 2 Session #8 Graph DFS , BFS [Part 1]/P - Continents.cpp
@@ -0,0 +1,99 @@
+// وَكَانَ فَضْلُ اللَّهِ عَلَيْكَ عَظِيمًا♡
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define all(x) x.begin(), x.end()
+#define input(vec) for (auto& i : vec) cin >> i
+#define inputone(vec , x) for (int i = 1 ; i <= x ; i++) cin >> vec[i]
+#define ABo_ZeiD ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+const ll N = 5005 , w = 1e6 + 5 , oo = 1e9;
+
+int dx[] = {0 , 0  , 1 , -1} ,
+    dy[] = {1 , -1 , 0 , 0};
+
+
+int n , m , x , y;
+vector <vector <char>> arr;
+bool vis[25][25];
+int cnt = 0; char lnd;
+
+
+bool can (int i , int j) {
+    return i >= 0 && i < n && j >= 0 && j < m && arr[i][j] == lnd && !vis[i][j];
+}
+
+void dfs (int l , int r) {
+    vis[l][r] = 1; cnt++;
+
+    for (int i = 0 ; i < 4 ; i++) {
+        int cX = (l + dx[i]) , cY = (r + dy[i]) % m;
+
+        if (cY < 0) cY = m - 1;
+
+        //cout << cX << " " << cY << '\n';
+
+        if (can(cX , cY)) {
+            dfs(cX , cY);
+        }
+    }
+}
+
+
+void solve () {
+
+    while (cin >> n >> m) {
+        arr.resize(n);
+
+        for (int i = 0 ; i < n ; i++) {
+            arr[i].resize(m);
+            for (int j = 0 ; j < m ; j++) {
+                cin >> arr[i][j];
+            }
+        }
+
+        cin >> x >> y;
+        lnd = arr[x][y];
+        dfs(x , y);
+        cnt = 0;
+
+        int mx = 0;
+
+        for (int i = 0 ; i < n ; i++) {
+            for (int j = 0 ; j < m ; j++) {
+                if (arr[i][j] == lnd && !vis[i][j]) {
+                    dfs(i , j);
+                    mx = max(mx , cnt); cnt = 0;
+                }
+            }
+        }
+
+        cout << mx << '\n';
+
+
+        arr.clear(); cnt = 0;
+        memset(vis , 0 , sizeof(vis));
+    }
+
+}
+
+int main() {
+    ABo_ZeiD; // i'm backkkkkk;
+
+//    memset(dp , -1 , sizeof(dp));
+    //freopen("smallest.in" , "r", stdin);
+//    pre();
+
+    int t = 1;
+//    cin >> t;
+    for (int i = 1 ; i <= t ; i++) {
+//        cout << "Case " << i << ": ";
+        solve();
+//        if (i != t) {
+//            cout << '\n';
+//        }
+    }
+
+
+    return 0;
+}
